3b9d892e70f0d1f2ef306d6dba54497f4b297a4f
Jassem01 <jassem.ayachi@hotmail.com>
2025-03-07 23:56:46 +0000

    add files to GitHub

---
+++ Models/Drivers.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TLE0.Models
+{
+    public class Drivers
+    {
+        public string earnings { get; set; }
+        public string email { get; set; }
+        public string id { get; set; }
+        public string name { get; set; }
+        public string phone { get; set; }
+        public string ratings { get; set; }
+    }
+}
---
+++ Models/Users.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TLE0.Models
+{
+    public class Users
+    {
+        public string email { get; set; }
+        public string id { get; set; }
+        public string name { get; set; }
+
+        public string phone { get; set; }
+    }
+}
